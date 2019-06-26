5e8b9312b6c01d41ab47a8f7148bf73eed4c9760
Jesse MacFadyen <purplecabbage@gmail.com>
2019-06-26 02:06:39 -0700

    allow single commands to be not tracked with --no-telemetry, added debug output

--- src/hooks/command-not-found.js
+++ src/hooks/command-not-found.js
@@ -1,8 +1,10 @@
 
 const telemetryLib = require('../telemetry-lib')
+const debug = require('debug')('aio-cli-plugin-telemetry')
 
 module.exports = async function (opts) {
   if (telemetryLib.isEnabled()) {
+    debug('tracking command-not-found =>', opts.id )
     telemetryLib.trackEvent('command-not-found', opts.id)
   }
 }
--- src/hooks/prerun.js
+++ src/hooks/prerun.js
@@ -1,8 +1,16 @@
 
 const telemetryLib = require('../telemetry-lib')
+const debug = require('debug')('aio-cli-plugin-telemetry')
 
 module.exports = async function (opts) {
+  if(opts.argv.indexOf('--no-telemetry') > -1) {
+    debug('--no-telemetry flag found. This command will not be tracked.')
+    opts.argv.splice(opts.argv.indexOf('--no-telemetry'),1)
+    telemetryLib.disableForCommand()
+  }
+
   if (telemetryLib.isEnabled()) {
+    debug('telemetry - prerun hook =>', opts.Command.id)
     telemetryLib.trackEvent('prerun',
       opts.Command.id,
       opts.argv.filter(arg => arg.indexOf('-') === 0).join(','))
