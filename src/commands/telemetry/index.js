3d316cb26f77fd225dd18c06f6c4e698176b97d8
Jesse MacFadyen <purplecabbage@gmail.com>
2022-02-14 11:40:16 -0800

    added license headers

--- src/commands/telemetry/index.js
+++ src/commands/telemetry/index.js
@@ -1,10 +1,21 @@
+/*
+Copyright 2022 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
 const {Command, flags} = require('@oclif/command')
 const telemetryLib = require('../../telemetry-lib')
 
 class IndexCommand extends Command {
   async run() {
     const {args, flags} = this.parse(IndexCommand)
-
+0
     if (flags.reset) {
       telemetryLib.reset()
       this.log('resetting telemetry')
