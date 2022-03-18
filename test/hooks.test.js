ccbd3ac7040ad36124641dc4f1de9a4a19e290c3
Jesse MacFadyen <purplecabbage@gmail.com>
2022-03-18 14:39:15 -0700

    tests round 1

---
+++ test/hooks.test.js
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2022 Adobe Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+const fetch = require('node-fetch')
+const inquirer = require('inquirer')
+const { stdout } = require('stdout-stderr')
+const { string } = require('@oclif/command/lib/flags')
+
+jest.mock('inquirer')
+
+
+
+
+
+describe('hook interfaces', () => {
+
+    beforeEach(() => {
+        fetch.mockReset()
+    })
+
+    test('command-error', async () => {
+        const hook = require('../src/hooks/command-error')
+        expect(typeof hook).toBe('function')
+        let res = await hook({message:'msg'})
+        expect(fetch).toHaveBeenCalledWith(expect.any(String),
+          expect.objectContaining({ body: expect.stringContaining('\"_adobeio\":{\"eventType\":\"command-error\"')}))
+    })
+
+    test('command-not-found', async () => {
+        const hook = require('../src/hooks/command-not-found')
+        expect(typeof hook).toBe('function')
+        let res = await hook({id:'id'})
+        expect(fetch).toHaveBeenCalledWith(expect.any(String),
+          expect.objectContaining({ body: expect.stringContaining('\"_adobeio\":{\"eventType\":\"command-not-found\"')}))
+    })
+
+    // test('init', async () => {
+    //     const hook = require('../src/hooks/init')
+    //     expect(typeof hook).toBe('function')
+    //     let res = await hook({config:{ name: 'name', version: '0.0.1'}})
+    //     expect(fetch).not.toHaveBeenCalled()
+    // })
+
+    test('postrun', async () => {
+        const hook = require('../src/hooks/postrun')
+        expect(typeof hook).toBe('function')
+        let res = await hook({Command:{id:'id'}, argv:['--hello']})
+        expect(fetch).toHaveBeenCalledWith(expect.any(String),
+          expect.objectContaining({ body: expect.stringContaining('\"_adobeio\":{\"eventType\":\"postrun\"')}))
+    })
+
+    test('prerun', async () => {
+        const hook = require('../src/hooks/prerun')
+        expect(typeof hook).toBe('function')
+        let res = await hook({Command:{id:'id'}, argv:['--hello']})
+        expect(fetch).not.toHaveBeenCalled()
+        res = await hook({Command:{id:'id'}, argv:['--hello', '--no-telemetry']})
+        expect(fetch).not.toHaveBeenCalled()
+    }) 
+
+    test('telemetry', async () => {
+        const hook = require('../src/hooks/telemetry')
+        expect(typeof hook).toBe('function')
+        let res = await hook({message:'msg'})
+        expect(fetch).toHaveBeenCalledWith(expect.any(String),
+          expect.objectContaining({ body: expect.stringContaining('\"_adobeio\":{\"eventType\":\"telemetry\"')}))
+    })
+})
---
+++ test/index.test.js
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2022 Adobe Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+const fetch = require('node-fetch')
+const inquirer = require('inquirer')
+const TheCommand = require('../src/commands/telemetry')
+const { stdout } = require('stdout-stderr')
+
+jest.mock('inquirer')
+
+let command
+
+beforeEach(() => {
+  fetch.resetMocks()
+  command = new TheCommand([])
+  fetch.mockResponseOnce('ok')
+})
+
+test('exports a run function', async () => {
+    expect(typeof TheCommand.run).toEqual('function')
+})
+
+describe('telemetry command', () => {
+  test('telemetry yes', async () => {
+    command.argv = ['yes']
+    await command.run()
+    expect(stdout.output).toMatch('Telemetry is on')
+  })
+
+  test('telemetry (get) on', async () => {
+    command.argv = []
+    await command.run()
+    expect(stdout.output).toMatch('Telemetry is on')
+  })
+
+  test('telemetry no', async () => {
+    command.argv = ['no']
+    await command.run()
+    expect(stdout.output).toMatch('Telemetry is off')
+  })
+
+  test('telemetry (get) off', async () => {
+    command.argv = []
+    await command.run()
+    expect(stdout.output).toMatch('Telemetry is off')
+  })
+
+  test('telemetry on', async () => {
+    command.argv = ['on']
+    await command.run()
+    expect(stdout.output).toMatch('Telemetry is on')
+  })
+
+  test('telemetry off', async () => {
+    command.argv = ['off']
+    await command.run()
+    expect(stdout.output).toMatch('Telemetry is off')
+  })
+
+  test('telemetry --reset', async () => {
+    command.argv = ['--reset']
+    await command.run()
+    expect(stdout.output).toMatch('resetting telemetry')
+  })
+
+})
