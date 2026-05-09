3e1701dd093a1a902db2b36823c74f0f2fd0d62a
Jesse MacFadyen <purplecabbage@gmail.com>
2026-05-08 17:54:18 -0700

    remove extra logging flag

--- src/flush-worker.js
+++ src/flush-worker.js
@@ -32,8 +32,7 @@ const { readQueue, writeQueue, clearQueue } = require('./queue-store')
 const fetch = createFetch()
 
 const DEFAULT_HEADERS = {
-  'Content-Type': 'application/json',
-  'x-ow-extra-logging': 'on'
+  'Content-Type': 'application/json'
 }
 
 /**
